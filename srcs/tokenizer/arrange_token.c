a2b13e16f7e6c2f3d9fba4ecfb0acad8c60fc376
takira <ca.fie.0220@gmail.com>
2023-01-19 21:35:34 +0900

    [#4]Add implement parenthisis validatiohn

--- srcs/tokenizer/arrange_token.c
+++ srcs/tokenizer/arrange_token.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:07:57 by takira            #+#    #+#             */
-/*   Updated: 2023/01/19 19:14:32 by takira           ###   ########.fr       */
+/*   Updated: 2023/01/19 21:34:35 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static void	delete_empty_elem(t_list **tokenlist_head);
 static int	valid_and_set_control_operator(t_list **tokenlist_head);
 static int	validate_syntax(t_list *tokenlist_head);
+static int	validate_subshell(t_list *tokenlist_head);
 static void	set_elem_type_if_operator(t_list **tokenlist_head);
 static int	set_elem_type_if_word(t_list **tokenlist_head);
 
@@ -29,6 +30,8 @@ int	arrange_and_validate_token_list(t_list **tokenlist_head)
 	set_elem_type_if_operator(tokenlist_head);
 	if (validate_quote(*tokenlist_head) == FAILURE)
 		return (FAILURE);
+	if (validate_subshell(*tokenlist_head) == FAILURE)
+		return (FAILURE);
 	debug_print_token_word(*tokenlist_head, "set opes");
 	delete_empty_elem(tokenlist_head);
 	if (validate_syntax(*tokenlist_head) == FAILURE)
@@ -38,6 +41,34 @@ int	arrange_and_validate_token_list(t_list **tokenlist_head)
 	if (ft_lstsize(*tokenlist_head) == 0)
 		return (FAILURE);
 	return (SUCCESS);
+}
+
+static int validate_subshell(t_list *tokenlist_head)
+{
+	t_list			*node;
+	t_token_elem	*token;
+	int				cnt;
+
+	if (!tokenlist_head)
+		return (FAILURE);
+	node = tokenlist_head;
+	cnt = 0;
+	while (node)
+	{
+		token = node->content;
+		if (token->type == e_subshell_start)
+			cnt++;
+		if (token->type == e_subshell_end)
+			cnt--;
+		node = node->next;
+	}
+	if (cnt == 0)
+		return (SUCCESS);
+	if (cnt > 0)
+		ft_dprintf(STDERR_FILENO, "minishell: unclosed parenthesis `('\n");
+	else
+		ft_dprintf(STDERR_FILENO, "minishell: unclosed parenthesis `)'\n");
+	return (FAILURE);
 }
 
 static int validate_syntax(t_list *tokenlist_head)
@@ -57,10 +88,8 @@ static int validate_syntax(t_list *tokenlist_head)
 			next_token = node->next->content;
 		else
 			next_token = NULL;
-
 		if (validate_context(token, next_token, is_head) == FAILURE)
 			return (FAILURE);
-
 		node = node->next;
 		if (is_head)
 			is_head = false;
