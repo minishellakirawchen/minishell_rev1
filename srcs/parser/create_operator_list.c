810c9491636cb8d22f09d31abbdcf7813db08849
takira <ca.fie.0220@gmail.com>
2023-01-24 18:44:50 +0900

    [#5]add for pull request

--- srcs/parser/create_operator_list.c
+++ srcs/parser/create_operator_list.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 17:06:20 by takira            #+#    #+#             */
-/*   Updated: 2023/01/24 18:12:28 by takira           ###   ########.fr       */
+/*   Updated: 2023/01/24 18:44:10 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,52 +111,6 @@ int	create_operator_list(t_info *info)
 	}
 	return (SUCCESS);
 }
-
-//t_exec_list	*create_operator_list(t_list **tokenlist_head)
-//{
-//	t_list			*popped_node;
-//	t_token_elem	*token_elem;
-//
-//	t_exec_list		*exec_head;
-//	t_exec_list		*pipeline_node;
-//	t_exec_list		*operator_node;
-//	ssize_t			subshell_depth;
-//
-//	if (!tokenlist_head || !*tokenlist_head)
-//		return (NULL);
-//	exec_head = create_execlist_node(e_node_head, NULL, NULL, NULL);
-//	pipeline_node = create_execlist_node(e_node_pipeline, NULL, &exec_head, NULL);
-//
-//	if (!exec_head || !pipeline_node)
-//		return (NULL); //TODO:all free
-//
-//	token_elem = (*tokenlist_head)->content;
-//	subshell_depth = token_elem->depth;
-//	while (*tokenlist_head)
-//	{
-//		popped_node = ft_lstpop(&(*tokenlist_head));
-//		token_elem = popped_node->content;
-//		if (!(is_tokentype_operator(token_elem->type) && token_elem->depth == subshell_depth))
-//			ft_lstadd_back(&(pipeline_node->token_list_head), popped_node);
-//		else
-//		{
-//			operator_node = create_execlist_node(e_node_operator, NULL, &pipeline_node, NULL);
-//			if (!operator_node)
-//				return (perror_ret_nullptr("malloc")); // TODO:all free
-//			operator_node->token_type = token_elem->type; //for debug print
-//			ft_lstdelone(popped_node, free_token_elem);
-//			popped_node = NULL;
-//			if (*tokenlist_head)
-//			{
-//				pipeline_node = create_execlist_node(e_node_pipeline, NULL, &operator_node, NULL);
-//				if (!pipeline_node)
-//					return (perror_ret_nullptr("malloc")); // TODO:all free
-//			}
-//		}
-//	}
-//	return (exec_head);
-//}
-
 
 t_exec_list *create_execlist_node(t_node_kind kind, t_list *token_head, t_exec_list **prev, t_exec_list **next)
 {
