7988fb2d60cb60cb2e3d6baba819f915a8152782
takira <ca.fie.0220@gmail.com>
2023-02-04 19:14:20 +0900

    [#19]Update tokenize

--- srcs/tokenizer/space_split.c
+++ srcs/tokenizer/space_split.c
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 16:36:28 by takira            #+#    #+#             */
-/*   Updated: 2023/02/04 19:04:42 by takira           ###   ########.fr       */
+/*   Updated: 2023/02/04 19:14:04 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /* FREE OK */
@@ -16,11 +16,27 @@ static t_split_info	init_split_info(const char *src, const char *delim, const ch
 static t_token_elem	*create_word_token_from_src(t_split_info *s_info);
 static char *get_trimmed_word(t_split_info *s_info);
 
+t_list_bdi	*get_new_splitted_token_node(t_split_info *s_info)
+{
+	t_list_bdi		*new_list;
+	t_token_elem	*new_token;
+
+	new_token = create_word_token_from_src(s_info);
+	if (!new_token)
+		return (perror_ret_nullptr("malloc"));
+	new_list = ft_lstnew_bdi((void *)new_token);
+	if (!new_list)
+	{
+		free_token_elem(new_token);
+		return (perror_ret_nullptr("malloc"));
+	}
+	return (new_list);
+}
+
 t_list_bdi	*get_delim_splitted_tokenlist(const char *src, const char *delim, const char *setchars)
 {
 	t_list_bdi		*tokenlist_head;
 	t_list_bdi		*new_list;
-	t_token_elem	*new_token;
 	t_split_info	s_info;
 
 	tokenlist_head = NULL;
@@ -29,16 +45,9 @@ t_list_bdi	*get_delim_splitted_tokenlist(const char *src, const char *delim, con
 	s_info = init_split_info(src, delim, setchars);
 	while (src[s_info.head_idx])
 	{
-		new_token = create_word_token_from_src(&s_info);
-		if (!new_token)
-		{
-			ft_lstclear_bdi(&tokenlist_head, free_token_elem);
-			return (perror_ret_nullptr("malloc"));
-		}
-		new_list = ft_lstnew_bdi((void *)new_token);
+		new_list = get_new_splitted_token_node(&s_info);
 		if (!new_list)
 		{
-			free_token_elem(new_token);
 			ft_lstclear_bdi(&tokenlist_head, free_token_elem);
 			return (perror_ret_nullptr("malloc"));
 		}
@@ -47,32 +56,42 @@ t_list_bdi	*get_delim_splitted_tokenlist(const char *src, const char *delim, con
 	return (tokenlist_head);
 }
 
+char *get_splitted_word(t_split_info *s_info)
+{
+	char			*word;
+
+	s_info->is_quoted = false;
+	while (s_info->src[s_info->head_idx] && is_chr_in_str(s_info->src[s_info->head_idx], s_info->delim))
+		s_info->head_idx += 1;
+	if (!s_info->src[s_info->head_idx])
+		word = ft_strdup("");
+	else
+	{
+		word = get_trimmed_word(s_info);
+		s_info->head_idx += s_info->word_len;
+	}
+	if (!word)
+		return(perror_ret_nullptr("malloc"));
+	return (word);
+}
+
 /* explain here */
-static t_token_elem	*create_word_token_from_src(t_split_info *split)
+static t_token_elem	*create_word_token_from_src(t_split_info *s_info)
 {
 	t_token_elem	*new_token;
 	char			*word;
 
-	split->is_quoted = false;
-	while (split->src[split->head_idx] && is_chr_in_str(split->src[split->head_idx], split->delim))
-		split->head_idx += 1;
-	if (!split->src[split->head_idx])
-		word = ft_strdup("");
-	else
-	{
-		word = get_trimmed_word(split);
-		split->head_idx += split->word_len;
-	}
+	word = get_splitted_word(s_info);
 	if (!word)
 		return (perror_ret_nullptr("malloc"));
-	new_token = create_token_elem(word, split->is_connect_to_next_word, split->is_quoted, '\0');
+	new_token = create_token_elem(word, s_info->is_connect_to_next_word, s_info->is_quoted, '\0');
 	if (!new_token)
 	{
 		free_1d_alloc(word);
 		free_token_elem(new_token);
 		return (perror_ret_nullptr("malloc"));
 	}
-	if (split->is_quoted && new_token->word)
+	if (s_info->is_quoted && new_token->word)
 		new_token->quote_chr = new_token->word[0];
 	return (new_token);
 }
